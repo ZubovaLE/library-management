aa3956677d260274a7171d5b90b1214dc98fd6c7
ZubovaLE <n.lubovj@gmail.com>
2026-09-24 06:51:12 +0300

    Centralize MapStruct configuration

--- src/main/java/com/zubova/library/author/AuthorMapper.java
+++ src/main/java/com/zubova/library/author/AuthorMapper.java
@@ -3,13 +3,11 @@ package com.zubova.library.author;
 import com.zubova.library.author.dto.AuthorDto;
 import com.zubova.library.author.dto.AuthorRequestDto;
 import com.zubova.library.common.mapping.BaseDtoMapper;
+import com.zubova.library.common.mapping.CentralMapperConfig;
 import org.mapstruct.Mapper;
-import org.mapstruct.MappingConstants;
 import org.mapstruct.MappingTarget;
 
-@Mapper(
-        componentModel = MappingConstants.ComponentModel.SPRING
-)
+@Mapper(config = CentralMapperConfig.class)
 public interface AuthorMapper extends BaseDtoMapper<Author, AuthorDto> {
 
     Author toEntity(AuthorRequestDto request);
--- src/main/java/com/zubova/library/book/BookMapper.java
+++ src/main/java/com/zubova/library/book/BookMapper.java
@@ -5,13 +5,14 @@ import com.zubova.library.author.AuthorMapper;
 import com.zubova.library.book.dto.BookDto;
 import com.zubova.library.book.dto.BookRequestDto;
 import com.zubova.library.common.mapping.BaseDtoMapper;
-import org.mapstruct.*;
+import com.zubova.library.common.mapping.CentralMapperConfig;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(
-        componentModel = MappingConstants.ComponentModel.SPRING,
-        injectionStrategy = InjectionStrategy.CONSTRUCTOR,
-        uses = AuthorMapper.class,
-        unmappedTargetPolicy = ReportingPolicy.ERROR
+        config = CentralMapperConfig.class,
+        uses = AuthorMapper.class
 )
 public interface BookMapper extends BaseDtoMapper<Book, BookDto> {
 
