09c430a40dc3590c2cfc635e42da1a9e658742b7
ZubovaLE <n.lubovj@gmail.com>
2026-09-25 10:28:34 +0300

    Improve OpenAPI response documentation

--- src/main/java/com/zubova/library/common/crud/CrudController.java
+++ src/main/java/com/zubova/library/common/crud/CrudController.java
@@ -1,9 +1,15 @@
 package com.zubova.library.common.crud;
 
 import com.zubova.library.common.application.CrudApplicationService;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,28 +20,86 @@ public abstract class CrudController<C, D> {
     private final CrudApplicationService<C, D> crudApplicationService;
 
     @GetMapping
+    @ApiResponse(
+            responseCode = "200",
+            description = "Resources retrieved"
+    )
     public List<D> getAll() {
         return crudApplicationService.getAll();
     }
 
     @GetMapping("/{id}")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Resource found"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Resource not found",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_PROBLEM_JSON_VALUE,
+                            schema = @Schema(implementation = ProblemDetail.class)
+                    )
+            )
+    })
     public D getById(@PathVariable Long id) {
         return crudApplicationService.getById(id);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Resource created"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid request",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_PROBLEM_JSON_VALUE,
+                            schema = @Schema(implementation = ProblemDetail.class)
+                    )
+            )
+    })
     public D create(@Valid @RequestBody C request) {
         return crudApplicationService.create(request);
     }
 
     @PutMapping("/{id}")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Resource updated"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid request",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_PROBLEM_JSON_VALUE,
+                            schema = @Schema(implementation = ProblemDetail.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Resource not found",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_PROBLEM_JSON_VALUE,
+                            schema = @Schema(implementation = ProblemDetail.class)
+                    )
+            )
+    })
     public D update(@PathVariable Long id, @Valid @RequestBody C request) {
         return crudApplicationService.update(id, request);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ApiResponse(
+            responseCode = "204",
+            description = "Resource deleted"
+    )
     public void delete(@PathVariable Long id) {
         crudApplicationService.delete(id);
     }
